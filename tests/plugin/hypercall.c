288f3ae38481ef4eaec911633ef5db6b2c3f2ba9
Chenzhuo Zhu <zcz95116@hotmail.com>
2020-02-11 17:11:02 -0800

    Add memory callback.

--- tests/plugin/hypercall.c
+++ tests/plugin/hypercall.c
@@ -17,13 +17,49 @@
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
+static bool mem_cb_on = false;
+static uint64_t mem_count = 0;
+static enum qemu_plugin_mem_rw rw = QEMU_PLUGIN_MEM_RW;
+
 static void vcpu_syscall_cb(qemu_plugin_id_t id, unsigned int vcpu_index,
                             int64_t num, uint64_t a1, uint64_t a2,
                             uint64_t a3, uint64_t a4, uint64_t a5,
                             uint64_t a6, uint64_t a7, uint64_t a8)
 {
-    if (num == HYPERCALL_NUM)
-        fprintf(stderr, "Hypercall\n");
+    if (num == HYPERCALL_NUM) {
+        mem_cb_on = !mem_cb_on;
+        fprintf(stderr, "Mem CB mode toggled.\n");
+    }
+}
+
+static void vcpu_mem(unsigned int cpu_index, qemu_plugin_meminfo_t meminfo,
+                     uint64_t vaddr, void *udata)
+{
+    if (!mem_cb_on)
+        return;
+
+    struct qemu_plugin_hwaddr *hwaddr;
+    hwaddr = qemu_plugin_get_hwaddr(meminfo, vaddr);
+    uint64_t paddr = qemu_plugin_hwaddr_device_offset(hwaddr);
+    mem_count++;
+    if (mem_count % 1000 == 0)
+        fprintf(stderr, "%c 0x%" PRIx64 " 0x%" PRIx64 "\n",
+                qemu_plugin_mem_is_store(meminfo)? 'w' : 'r', vaddr, paddr);
+}
+
+
+
+static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
+{
+    size_t n = qemu_plugin_tb_n_insns(tb);
+    size_t i;
+
+    for (i = 0; i < n; i++) {
+        struct qemu_plugin_insn *insn = qemu_plugin_tb_get_insn(tb, i);
+        qemu_plugin_register_vcpu_mem_cb(insn, vcpu_mem,
+                                         QEMU_PLUGIN_CB_NO_REGS,
+                                         rw, NULL);
+    }
 }
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
@@ -31,5 +67,7 @@ QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
                                            int argc, char **argv)
 {
     qemu_plugin_register_vcpu_syscall_cb(id, vcpu_syscall_cb);
+    qemu_plugin_register_vcpu_tb_trans_cb(id, vcpu_tb_trans);
+    // qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
     return 0;
 }
