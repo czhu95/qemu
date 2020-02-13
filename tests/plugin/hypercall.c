03494d668710d8c5e47f4414b3fb216292d3d787
Chenzhuo Zhu <zcz95116@hotmail.com>
2020-02-12 17:55:13 -0800

    Rewrite memory pring as hypershell command pypersh.

--- tests/plugin/hypercall.c
+++ tests/plugin/hypercall.c
@@ -13,45 +13,96 @@
 
 #include <qemu-plugin.h>
 
-#define HYPERCALL_NUM 777
+#define HYPERCALL_MAGIC 712
 
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
-static bool mem_cb_on = false;
-static uint64_t mem_count = 0;
 static enum qemu_plugin_mem_rw rw = QEMU_PLUGIN_MEM_RW;
-static char buf[20] = "";
+static char buf[128] = "";
+
+/* ------------------------------------------------------------------------- */
+/* pmem hypercall. */
+#define HC_PMEM_READ        0x1
+#define HC_PMEM_WRITE       0x2
+#define HC_PMEM_KERNEL      0x4
+#define HC_PMEM_USER        0x8
+typedef uint8_t hc_pmem_attr_t;
+
+static bool hc_pmem_on = false;
+static uint32_t hc_pmem_int;
+static hc_pmem_attr_t hc_pmem_attr;
+static FILE *hc_pmem_file;
+static uint32_t hc_pmem_count;
+
+static void hypercall_pmem(unsigned int cpu_id, qemu_plugin_meminfo_t meminfo,
+                           uint64_t vaddr)
+{
+    struct qemu_plugin_hwaddr *hwaddr;
+    uint64_t paddr;
+    bool is_store;
+    bool is_kernel;
+    hc_pmem_attr_t attr;
+
+    if (hc_pmem_count++ % hc_pmem_int == 0) {
+        hwaddr = qemu_plugin_get_hwaddr(meminfo, vaddr);
+        paddr = qemu_plugin_hwaddr_device_offset(hwaddr);
+
+        is_store = qemu_plugin_mem_is_store(meminfo);
+        is_kernel = qemu_plugin_in_kernel();
+
+        attr = (is_store ? HC_PMEM_WRITE : HC_PMEM_READ) |
+               (is_kernel ? HC_PMEM_KERNEL : HC_PMEM_USER);
+
+        if (hc_pmem_attr & attr)
+            fprintf(hc_pmem_file,
+                    "%u %c %c 0x%016" PRIx64 " 0x%08" PRIx64 "\n", cpu_id,
+                    is_kernel ? 'k' : 'u', is_store ? 'w' : 'r', vaddr, paddr);
+    }
+
+}
+/* ------------------------------------------------------------------------- */
 
 static void vcpu_syscall_cb(qemu_plugin_id_t id, unsigned int vcpu_index,
                             int64_t num, uint64_t a1, uint64_t a2,
                             uint64_t a3, uint64_t a4, uint64_t a5,
                             uint64_t a6, uint64_t a7, uint64_t a8)
 {
-    if (num == HYPERCALL_NUM) {
-        mem_cb_on = !mem_cb_on;
-        if (!qemu_plugin_virt_mem_rw(a1, buf, 19, false, false)) {
+    char *hc, *arg;
+
+    if (num == HYPERCALL_MAGIC) {
+        if (!qemu_plugin_virt_mem_rw(a1, buf, 127, false, false)) {
             fprintf(stderr,
                     "Fail to read guest virtual memory 0x%" PRIx64 "\n", a1);
+            return;
         }
-        fprintf(stderr, "guest> %s\n", buf);
+        fprintf(stderr, "guest> %s", buf);
+
+        hc = strtok(buf, " \n");
+        if (hc == NULL)
+            return;
+
+        if (!strcmp(hc, "pmem")) {
+            arg = strtok(NULL, " \n");
+            if (arg && !strcmp(arg, "on")) {
+                hc_pmem_on = true;
+                hc_pmem_int = 10000;
+                hc_pmem_attr = 0xF;
+                hc_pmem_file = stderr;
+                hc_pmem_count = 0;
+            } else if (arg && !strcmp(arg, "off")) {
+                hc_pmem_on = false;
+            } else {
+                fprintf(stderr, "Unknown argument for pmem\n");
+            }
+        }
     }
 }
 
 static void vcpu_mem(unsigned int cpu_index, qemu_plugin_meminfo_t meminfo,
                      uint64_t vaddr, void *udata)
 {
-    if (!mem_cb_on)
-        return;
-
-    if (mem_count++ % 10000 == 0) {
-        struct qemu_plugin_hwaddr *hwaddr;
-        hwaddr = qemu_plugin_get_hwaddr(meminfo, vaddr);
-        uint64_t paddr = qemu_plugin_hwaddr_device_offset(hwaddr);
-
-        fprintf(stderr, "%c %c 0x%" PRIx64 " 0x%" PRIx64 "\n",
-                qemu_plugin_in_kernel() ? 'k' : 'u',
-                qemu_plugin_mem_is_store(meminfo) ? 'w' : 'r', vaddr, paddr);
-    }
+    if (hc_pmem_on)
+        hypercall_pmem(cpu_index, meminfo, vaddr);
 }
 
 
