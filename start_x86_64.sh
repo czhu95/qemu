422c941d1b6c287b05f22e84dbb68b59abf7c5f2
Chenzhuo Zhu <zcz95116@hotmail.com>
2020-02-18 14:41:21 -0800

    Add options for cores and debug.

--- start_x86_64.sh
+++ start_x86_64.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
+CORES=2
 
-QEMU=./build/x86_64-softmmu/qemu-system-x86_64
+while getopts "dn:" opt; do
+    case "$opt" in
+        d)
+            debug=1
+            ;;
+        n)
+            CORES=$OPTARG
+            ;;
+    esac
+done
+
+if [ -z $debug ]
+then
+    QEMU=./build/x86_64-softmmu/qemu-system-x86_64
+    PLUGIN=./hypersh/host/libhypersh.so
+else
+    QEMU=./build/debug/x86_64-softmmu/qemu-system-x86_64
+    PLUGIN=./hypersh/host/libhypersh.sod
+fi
+
 HDA=../panda/images/xpsp3.qcow2
 VNC=0.0.0.0:5900
+MEM=${CORES}G
+SMP=${CORES},sockets=${CORES}
+for c in $(seq 0 $((CORES-1))); do
+    SMP+=" -object memory-backend-ram,id=mem${c},size=1G"
+    SMP+=" -numa node,nodeid=${c},memdev=mem${c}"
+    SMP+=" -numa cpu,node-id=${c},socket-id=${c}"
+done
 
-${QEMU} -hda ${HDA} -vnc ${VNC} -plugin ./hypersh/host/libhypersh.so -m 2G -smp 2,sockets=2 \
-    -object memory-backend-ram,id=mem0,size=1G \
-    -object memory-backend-ram,id=mem1,size=1G \
-    -numa node,nodeid=0,memdev=mem0 -numa node,nodeid=1,memdev=mem1 \
-    -numa cpu,node-id=0,socket-id=0 -numa cpu,node-id=1,socket-id=1
+QEMU_ARGS="-hda ${HDA} -vnc ${VNC} -plugin ${PLUGIN} -m ${MEM} -smp ${SMP}"
+echo ${QEMU} ${QEMU_ARGS}
+
+if [ -z $debug ]
+then
+    ${QEMU} ${QEMU_ARGS}
+else
+    gdb --args ${QEMU} ${QEMU_ARGS}
+fi
