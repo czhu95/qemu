c834da1f85d3eef04a3c22c8ae05dd61466b8ae1
Chenzhuo Zhu <czhu95@stanford.edu>
2020-05-26 12:15:39 -0700

    Update start script for multi-node deployment.

--- start_x86_64.sh
+++ start_x86_64.sh
@@ -26,14 +26,15 @@ else
     PLUGIN_SUFFIX="d"
 fi
 
-HDA=../panda/images/xpsp3.qcow2
+HDA=./images/xpsp3.qcow2
 VNC=0.0.0.0:5900
-MEM=${NODES}G
 SMP=${CORES},sockets=${NODES}
 CPUS_PER_NODE=$((CORES/NODES))
+MEM_PER_NODE=16
+MEM=$((NODES*MEM_PER_NODE))
 for n in $(seq 0 $((NODES-1))); do
     CPUS=$((CPUS_PER_NODE*n))-$((CPUS_PER_NODE*(n+1)-1))
-    SMP+=" -object memory-backend-ram,id=mem${n},size=1G"
+    SMP+=" -object memory-backend-ram,id=mem${n},size=${MEM_PER_NODE}G"
     SMP+=" -numa node,nodeid=${n},memdev=mem${n},cpus=${CPUS}"
 done
 
@@ -41,7 +42,7 @@ for p in ${PLUGINS[@]}; do
     PLUGIN_ARGS+=" -plugin ./hypersh/host/lib${p}.so${PLUGIN_SUFFIX}"
 done
 
-QEMU_ARGS="-hda ${HDA} -vnc ${VNC} ${PLUGIN_ARGS} -m ${MEM} -smp ${SMP}"
+QEMU_ARGS="-hda ${HDA} -vnc ${VNC} ${PLUGIN_ARGS} -m ${MEM}G -smp ${SMP}"
 echo ${QEMU} ${QEMU_ARGS}
 
 if [ -z $debug ]; then
